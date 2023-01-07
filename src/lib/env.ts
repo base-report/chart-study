918a82b1eca39a1e13a7e5859acfb14921187d54
e0 <shan.huang@me.com>
2023-01-07 20:13:00 +0100

    refactor: use sveltekit style env vars

--- src/lib/env.ts
+++
@@ -1,9 +0,0 @@
-import { browser } from '$app/environment';
-
-const getEnvVar = (name: string): string | boolean | undefined => {
-	return browser || (process !== undefined && process.env.NODE_ENV !== 'production')
-		? import.meta.env[name]
-		: process.env[name];
-};
-
-export const FMP_API_URL = getEnvVar('VITE_FMP_API_URL') as string;
--- src/lib/fmp/api.ts
+++ src/lib/fmp/api.ts
@@ -1,9 +1,8 @@
-import { FMP_API_URL } from '$lib/env';
-import { FMP_API_KEY } from '$lib/secrets';
+import { SECRET_FMP_API_URL, SECRET_FMP_API_KEY } from '$env/static/private';
 
 const api = async <T>(path: string, version = 3): Promise<T> => {
 	const prefix = path.includes('?') ? '&' : '?';
-	const url = `${FMP_API_URL}/v${version}/${path}${prefix}apikey=${FMP_API_KEY}`;
+	const url = `${SECRET_FMP_API_URL}/v${version}/${path}${prefix}apikey=${SECRET_FMP_API_KEY}`;
 
 	return fetch(url).then(async (response) => {
 		if (!response.ok) {
