db9eab87bf5035d2c8ae55b6f039590500d88bb2
e0 <shan.huang@me.com>
2023-01-24 00:11:25 +0100

    feat: hook up daily chart data with code runner

--- src/lib/components/widgets/code-editor/util.ts
+++ src/lib/components/widgets/code-editor/util.ts
@@ -1,3 +1,6 @@
+import { get } from 'svelte/store';
+import { chartData } from '$lib/store/timeseries';
+
 const DEFAULT_CODE = `/*
 Warning: This is a code editor that will run any code you type in here.
 Please make sure you trust the source of the code you are running.
@@ -11,8 +14,12 @@ const runCodeInWidget = (event: CustomEvent) => {
 		}
 	} = event.detail;
 	try {
-		const fn = new Function('widget', code.code);
-		fn();
+		const { daily } = get(chartData);
+		const fn = new Function('daily', code.code);
+		// TODO: add docs about daily in default code
+		// TODO: add docs about how to use the data in the code
+		// TODO: add docs about what to return from the code
+		fn(daily);
 	} catch (e) {
 		console.error(e);
 	}
